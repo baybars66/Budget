660f443427bba321242490fed9499f43b7ed7bc1
Baybars ÖNAL <bay66bars@gmail.com>
2021-01-15 16:34:42 +0300

    resimler 131 166

--- src/COMPONENTS/Imza.js
+++ src/COMPONENTS/Imza.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
  const Imza =()=> {
   
         return (
-            <div className=  "d-flex justify-content-end" style = { {font :"italic bold 10px arial,serif"}} >
+            <div className=  "d-flex justify-content-end" style = { {font :"italic bold 5px arial,serif"}} >
                 Written by Baybars Önal
             </div>
         )
